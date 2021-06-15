609751158900e68571028bf64db5425f6fa443ef
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2021-06-15 23:09:56 +1000

    Added tests for inverted index

---
+++ src/main/java/org/example/simplesearch/index/InvertedIndex.java
@@ -0,0 +1,34 @@
+package org.example.simplesearch.index;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Inverted index, containing word -> document ID mappings.
+ */
+public class InvertedIndex {
+
+  private final Map<String, Set<Integer>> index = new HashMap<>();
+
+  /**
+   * Adds a doc to the index, storing its ID against its field values.
+   *
+   * @param id    The ID of the document.
+   * @param value The document.
+   */
+  void addDoc(int id, String value) {
+
+  }
+
+  /**
+   * Retrieves all document IDs matching a value.
+   *
+   * @param val The value to search.
+   * @return The matching IDs.
+   */
+  Set<Integer> retrieveHits(String val) {
+    return Collections.emptySet();
+  }
+}
---
+++ src/test/java/org/example/simplesearch/index/InvertedIndexTest.java
@@ -0,0 +1,40 @@
+package org.example.simplesearch.index;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InvertedIndexTest {
+
+  @Test
+  void whenAddingDocWithStringProp_canBeRetrieved() {
+    InvertedIndex index = new InvertedIndex();
+    index.addDoc(1, "EXAMPLE");
+
+    Set<Integer> res = index.retrieveHits("EXAMPLE");
+    assertEquals(singleton(1), res);
+  }
+
+  @Test
+  void givenMultipleMatchingDocs_whenAddingDocWithStringProp_canBeRetrieved() {
+    InvertedIndex index = new InvertedIndex();
+    index.addDoc(1, "EXAMPLE");
+    index.addDoc(2, "EXAMPLE");
+
+    Set<Integer> res = index.retrieveHits("EXAMPLE");
+    assertEquals(Set.of(1, 2), res);
+  }
+
+  @Test
+  void whenSearchingWithNonMatchingProp_cannotBeRetrieved() {
+    InvertedIndex index = new InvertedIndex();
+    index.addDoc(1, "EXAMPLE");
+
+    Set<Integer> res = index.retrieveHits("NOT_MATCHING");
+    assertEquals(emptySet(), res);
+  }
+}
